98d777c62493425bfc574e9f0f80e53cec80a0c5
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2021-12-15 18:44:17 +0530

    Adds solution for day 14

--- bin/Main.hs
+++ bin/Main.hs
@@ -5,6 +5,7 @@ import qualified AoC.Day10 as Day10
 import qualified AoC.Day11 as Day11
 import qualified AoC.Day12 as Day12
 import qualified AoC.Day13 as Day13
+import qualified AoC.Day14 as Day14
 import qualified AoC.Day2 as Day2
 import qualified AoC.Day3 as Day3
 import qualified AoC.Day4 as Day4
@@ -29,7 +30,8 @@ solutions =
     (10, [Day10.part1, Day10.part2]),
     (11, [Day11.part1, Day11.part2]),
     (12, [Day12.part1, Day12.part2]),
-    (13, [Day13.part1, Day13.part2])
+    (13, [Day13.part1, Day13.part2]),
+    (14, [Day14.part1, Day14.part2])
   ]
 
 main :: IO ()
---
+++ src/AoC/Day14.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module AoC.Day14 where
+
+import AoC.Utils (freq, pairify, sliding)
+import Data.Bifunctor (second)
+import Data.Foldable (Foldable (toList))
+import Data.List (sortOn)
+import Data.List.Split (splitOn)
+import qualified Data.Map.Strict as Map
+
+type Template = String
+
+type Subs = Map.Map Template Char
+
+readInput :: String -> (Template, Subs)
+readInput =
+  second (Map.fromList . map (second head . pairify . splitOn " -> ") . lines)
+    . pairify
+    . splitOn "\n\n"
+
+solve :: Int -> String -> Map.Map Template Char -> Int
+solve stepCount template subs =
+  let pairCounts = go stepCount $ Map.fromList $ freq $ toList $ sliding 2 template
+      charCounts =
+        foldl
+          (\m (pair, count) -> foldl (insert count) m pair)
+          (foldl (insert 1) Map.empty [head template, last template])
+          $ Map.toList pairCounts
+      charCountList = sortOn snd $ Map.toList charCounts
+   in (snd (last charCountList) - snd (head charCountList)) `div` 2
+  where
+    go 0 pairCounts = pairCounts
+    go stepCount' pairCounts =
+      go (stepCount' - 1) $
+        foldl
+          (\counts (pair, count) -> foldl (insert count) counts $ expandPair pair)
+          Map.empty
+          $ Map.toList pairCounts
+
+    insert v m k = Map.insertWith (+) k v m
+
+    expandPair = \case
+      s@[a, b] -> maybe [s] (\c -> [[a, c], [c, b]]) $ Map.lookup s subs
+      _ -> error "expandPair: invalid input"
+
+part1 :: String -> String
+part1 = show . uncurry (solve 10) . readInput
+
+part2 :: String -> String
+part2 = show . uncurry (solve 40) . readInput
